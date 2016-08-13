e8a5a0d6a2d24613156ed452885f3020bccb15c3
Alston Lin <alstonlin812@gmail.com>
2016-08-12 22:57:54 -0400

    Refactored the entire lifecycle to account for permissions for newer Android versions, because Services + Permissions = dumb solutions

---
+++ app/src/main/java/io/alstonlin/thelearninglock/PermissionRequestActivity.java
@@ -0,0 +1,74 @@
+package io.alstonlin.thelearninglock;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.os.Bundle;
+
+import io.alstonlin.thelearninglock.lockscreen.LockScreenService;
+
+/**
+ * An Activity that's called from LockScreenService to Request a permission, and then
+ * delegate back to the Service. The only reason this exists is because startActivityForResult or
+ * onResume apparently not a thing for services. Yay, Android Dev!
+ */
+public class PermissionRequestActivity extends Activity {
+    public static final int OVERLAY_FLAG = 23;
+    public static final int NOTIFICATIONS_FLAG = 29;
+
+    private static final int OVERLAY_REQUEST_CODE = 5463;
+    private static final int NOTIFICATIONS_REQUEST_CODE = 5464;
+
+    private boolean requested = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int flag = getIntent().getFlags() - Intent.FLAG_ACTIVITY_NEW_TASK;
+        switch (flag){
+            case OVERLAY_FLAG:
+                requestOverlayPermission();
+                break;
+            case NOTIFICATIONS_FLAG:
+                requestNotificationsPermission();
+                break;
+            default:
+                throw new IllegalStateException("Can only start this activity with the " +
+                        "OVERLAY_FLAG or NOTIFICATIONS_FLAG flag!");
+        }
+    }
+
+    private void requestOverlayPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_REQUEST_CODE);
+        } else{
+            throw new IllegalStateException("Why is this Activity being started on an API level " + Build.VERSION.SDK_INT);
+        }
+    }
+
+    private void requestNotificationsPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS);
+            startActivityForResult(intent, NOTIFICATIONS_REQUEST_CODE);
+        } else{
+            throw new IllegalStateException("Why is this Activity being started on an API level " + Build.VERSION.SDK_INT);
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (requested) {
+            Intent intent = new Intent(this, LockScreenService.class);
+            intent.addFlags(LockScreenService.OPEN_SETUP_ACTIVITY);
+            startService(intent);
+            finish();
+        } else{
+            requested = true;
+        }
+    }
+}
--- app/src/main/java/io/alstonlin/thelearninglock/setup/SetupActivity.java
+++ app/src/main/java/io/alstonlin/thelearninglock/setup/SetupActivity.java
@@ -1,9 +1,5 @@
 package io.alstonlin.thelearninglock.setup;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -11,26 +7,19 @@ import android.support.v4.app.FragmentTransaction;
 
 import io.alstonlin.thelearninglock.FragmentChangable;
 import io.alstonlin.thelearninglock.R;
-import io.alstonlin.thelearninglock.setup.BackgroundPickerFragment;
 
+/**
+ * The activity that goes through the set up process of the lock screen.
+ */
 public class SetupActivity extends FragmentActivity implements FragmentChangable {
-    public final static int REQUEST_CODE = 5463;
     private Bundle savedInstanceState;
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkDrawOverlayPermission();
         this.savedInstanceState = savedInstanceState;
+        startApp();
     }
-
-    public void changeFragment(Fragment fragment){
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
 
     private void startApp(){
         setContentView(R.layout.activity_setup);
@@ -46,26 +35,10 @@ public class SetupActivity extends FragmentActivity implements FragmentChangable
         }
     }
 
-    /*
-        Permission Checks
-    */
-
-    private void checkDrawOverlayPermission() {
-        if (!Settings.canDrawOverlays(this) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, REQUEST_CODE);
-        } else {
-            startApp();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (requestCode == REQUEST_CODE && Settings.canDrawOverlays(this)) {
-                startApp();
-            }
-        }
+    public void changeFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
